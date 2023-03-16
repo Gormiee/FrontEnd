1dbc651b4dd068c3a4d900d5c19d7816a89e2922
Mathias Gorm Petersen <201303934@post.au.dk>
2023-03-16 15:19:35 +0100

    many things

---
+++ DABw6_EFcore/DABw6_Exercise27/MyDBContext.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DABw6_Exercise27
+{
+    public class MyDBContext : DbContext
+    {
+        private const string DbName = "EFGetStarted";
+        private const string ConnectionString = $"Data Source=localhost;Initial Catalog={DbName};User ID=SA;Password=123DOCKER@wsl>;Connect Timeout=30;Encrypt=False;Trust Server Certificate=False;Application Intent=ReadWrite;Multi Subnet Failover=False";
+
+        public DbSet<Blog> Blogs { get; set; }
+        public DbSet<Post> Posts { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder options)
+            => options.UseSqlServer(ConnectionString);
+        //recheck connectionString if issue connecting to db
+        //can reuse connection string from week 3 using your new database
+    }
+}
